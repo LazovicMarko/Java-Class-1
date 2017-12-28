5e22d07eb80ef3193955991f713f206f4f3b34a4
Marko Lazovic <marko.lazovic.15@singimail.rs>
2017-12-28 20:28:44 +0100

    Final

--- src/main/java/com/javaclass/first/exercises/PassByReference.java
+++ src/main/java/com/javaclass/first/exercises/PassByReference.java
@@ -1,5 +1,25 @@
 package com.javaclass.first.exercises;
 
 public class PassByReference {
-    int i;
+    private int size;
+    public static void main(String[] args) {
+        PassByReference pass = new PassByReference(5);
+        System.out.println("Java uses pass by value.But,");
+        System.out.println("When an object is passed as argument to a method, actually the reference to that object is passed.");
+        System.out.println("Size before change: " + pass.size);
+        change(pass);
+        System.out.println("Size after change: " + pass.size);
+    }
+
+    private static void change(PassByReference byReference){
+        byReference.setSize(100);
+    }
+
+    private PassByReference(int size) {
+        this.size = size;
+    }
+
+    private void setSize(int size) {
+        this.size = size;
+    }
 }
